cd3cfd251d875fd6cd5649365ef686b132f6ba5e
minhphucdev201 <minhphuc201.dev@gmail.com>
2022-08-22 15:37:29 +0700

    update router header app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { NotFound, PrivateRoute } from 'components/Common';
 import { AdminLayout } from 'components/Layout';
 import LoginPage from 'features/auth/pages/LoginPage';
 import { Counter } from 'features/counter/Counter';
+import { DashBoardFeature } from 'features/dashboard';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
@@ -13,12 +14,13 @@ function App() {
     <>
       {/* <Header /> */}
       <Switch>
-        <Route path="/" component={Counter} exact />
+        <Route path="/" component={AdminLayout} exact />
+        <Route path="/counter" component={Counter} />
         <Route path="/login">
           <LoginPage />
         </Route>
         <PrivateRoute path="/admin" component={AdminLayout} />
-        {/* <Route path="/dashboard" component={DashBoardFeature} /> */}
+        <Route path="/admin/dashboard" component={DashBoardFeature} />
         <Route component={NotFound} />
       </Switch>
     </>
